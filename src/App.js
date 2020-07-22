beca028278c1b98e2eb9a93bd8b6aaabf2914428
Francisco Gomes <francisco.gomes1@sainsburys.co.uk>
2020-07-22 16:41:40 +0100

    minor refactor and resized images

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import cristinaLogo from './images/cristina-logo.png'
-import dulceLogo from './images/dulce-logo.png'
-import './App.css';
+import cristinaLogo from "./images/cristina-logo.png";
+import dulceLogo from "./images/dulce-logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="background"/>
-          <div className="main">
-            <div className="main-overlay"/>
-              <div className="logos-container">
-                <img className="cristina-logo" src={cristinaLogo}/>
-                <img className="dulce-logo" src={dulceLogo}/>
-              </div>
-              <div className="info-container">
-                <h1 className="title">Website em construção</h1>
-                <h4 className="subtitle">Entre em contacto connosco</h4>
-                <div className="contacts-container">
-                <ul className="address-list">
-                  <li>Rua Santos Bernardes,</li>
-                  <li>Edif. Atlantic, n.15, Loja 2</li>
-                  <li>2560-362 Torres Vedras</li>
-                </ul>
-                <ul className="contact-list">
-                  <li>(+351) 261 064 229</li> 
-                  <li>cristina.gomes@ccg-advogados.pt</li>
-                  <li>dulcecesar.3194@gmail.com</li>
-                </ul>
-              </div>
+    <div className='App'>
+      <div className='background' />
+      <div className='main'>
+        <div className='main-overlay' />
+        <div className='logos-container'>
+          <img
+            className='cristina-logo'
+            src={cristinaLogo}
+            alt='cristina gomes logo'
+          />
+          <img className='dulce-logo' src={dulceLogo} alt='dulce cesar logo' />
+        </div>
+        <div className='info-container'>
+          <h1 className='title'>Website em construção</h1>
+          <h4 className='subtitle'>Entre em contacto connosco</h4>
+          <div className='contacts-container'>
+            <ul className='address-list'>
+              <li>Rua Santos Bernardes,</li>
+              <li>Edif. Atlantic, n.15, Loja 2</li>
+              <li>2560-362 Torres Vedras</li>
+            </ul>
+            <ul className='contact-list'>
+              <li>(+351) 261 064 229</li>
+              <li>cristina.gomes@ccg-advogados.pt</li>
+              <li>dulcecesar.3194@gmail.com</li>
+            </ul>
           </div>
+        </div>
       </div>
     </div>
   );
